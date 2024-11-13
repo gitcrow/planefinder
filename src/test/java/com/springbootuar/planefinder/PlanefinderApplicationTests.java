2111b4a1ef820cb3b25654ac0621d82465e86d28
최영민 <justdoithaha@gmail.com>
2024-11-13 16:46:42 +0900

    new

---
+++ src/test/java/com/springbootuar/planefinder/PlanefinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootuar.planefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanefinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
